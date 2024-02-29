f38e2a46f1dc9c6b0246a794dc0390b071de49d1
g.park <g.park@bravesoft.co.jp>
2024-02-29 11:22:04 +0900

    Initial Commit

---
+++ DragAndDrop/DragAndDropApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragAndDropApp.swift
+//  DragAndDrop
+//
+//  Created by パク　ギョンソク on 2024/02/29.
+//
+
+import SwiftUI
+
+@main
+struct DragAndDropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
